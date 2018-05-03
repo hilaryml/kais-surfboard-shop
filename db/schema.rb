ef15c1a8ea78079cddfe09c878f2583287d91d14
hilaryml <hilyttle@gmail.com>
2018-05-03 11:39:16 -0400

    adds category and surfboardcategory tables in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503153123) do
+ActiveRecord::Schema.define(version: 20180503153815) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "surfboard_categories", force: :cascade do |t|
+    t.integer  "surfboard_id"
+    t.integer  "category_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["category_id"], name: "index_surfboard_categories_on_category_id"
+    t.index ["surfboard_id"], name: "index_surfboard_categories_on_surfboard_id"
+  end
 
   create_table "surfboards", force: :cascade do |t|
     t.string   "name"
