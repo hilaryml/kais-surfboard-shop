0a8ad8f4e0aa2259695990d0064d9b9ba3b5f8ab
hilaryml <hilyttle@gmail.com>
2018-05-03 13:08:31 -0400

    adds show, create, and destroy methods to surfboardscontroller

--- app/controllers/api/surfboards_controller.rb
+++ app/controllers/api/surfboards_controller.rb
@@ -4,4 +4,32 @@ class Api::SurfboardsController < ApplicationController
     render json: Surfboard.order(:skill_level).to_json(include: [:categories])
   end
 
+  def show
+    render json: Surfboard.find_by(id: params[:id]).to_json(include: [:categories])
+  end
+
+  def create
+    surfboard = Surfboard.create(surfboard_params)
+    if surfboard.save
+      render json: { id: surfboard.id, success: 'ok' }
+    else
+      render json: { message: 'error' }, status: 412
+    end
+  end
+
+  def destroy
+    surfboard = Surfboard.find_by(id: params[:id])
+    if surfboard.delete
+      render json: { success: 'ok' }
+    else
+      render json: { message: 'error' }, status: 412
+    end
+  end
+
+  private
+
+  def surfboard_params
+    params.require(:surfboard).permit(:name, :lenth, :price, :quantity, category_ids: [])
+  end
+
 end
