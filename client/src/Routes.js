caec08fbe657f536f4955271f518460d835719c7
hilaryml <hilyttle@gmail.com>
2018-05-03 20:24:31 -0400

    adds home, about, navbar component code, adds links to navbar

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './components/App'
+import Surfboards from './components/Surfboards'
+import About from './components/About'
+import Home from './components/Home'
 
 export default (
-  <Routes path="/" component={App}>
+  <Route path="/" component={App}>
     <IndexRoute component={Home} />
-    <Route path="/surfboards" />
-    <Route path="/about" />
+    <Route path="/surfboards" component={Surfboards}/>
+    <Route path="/about" component={About} />
   </Route>
 )
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Kai's Surfboard Shop</h1>
+        <p>Check out our surfboards at <Link to="/surfboards">Surfboards</Link></p>
+      </div>
+    )
+  }
+}
