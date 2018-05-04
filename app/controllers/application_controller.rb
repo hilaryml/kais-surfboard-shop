ab3c695330c975a97123adf1b2616c3b55ec3480
hilaryml <hilyttle@gmail.com>
2018-05-03 21:24:31 -0400

    adds functions to surfboards.js, and adds index route to surfboards controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def index
+    render json: Surfboard.all
+  end
+
 end
--- client/src/components/Surfboards.js
+++ client/src/components/Surfboards.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Surfboards extends Component {
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+      surfboards = []
+    }
+  }
+
+  componentDidMount() {
+    return fetch('/api/surfboards', {
+      method: 'GET',
+      headers: {
+        'Accepts': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+
   render() {
+
+    cont surfboards = this.state.surfboards.map((surfboard) =>
+
+      <div>
+        <hi>{surfboard.name}</h1>
+        <p>Length: {surfboard.length} | Price: {surfboard.price} | Quantity: {surfboard.quantity}
+        </p>
+      </div>
+    )
+
     return (
       <div>Surfboards Component is coming</div>
     )
